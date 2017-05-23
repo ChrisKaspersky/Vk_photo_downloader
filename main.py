6fc8443aa0d9a40a5215d425c9d9f535b0e43116
ChrisKaspersky <fckthscty@gmail.com>
2017-05-23 20:06:50 +0300

    .

--- main.py
+++ main.py
@@ -1,40 +1,69 @@
+# coding=utf-8
 import vk
+import vk.exceptions
 import os
 import urllib.request
 
-print('Vvedite login')
-login_ = input()
-print('Vvedite parol')
-parol_ = input()
-print('Vvedite ssilku na besedu:')
-id_ = input()
-print('Skolko fotok?')
-num = int(input())
-links = []
-session = vk.AuthSession('5932434', login_, parol_, scope='wall, messages')
-vk_api = vk.API(session)
-start = ''
-range_ = num // 100
-for i in range(range_):
-    attachments = vk_api.messages.getHistoryAttachments(peer_id=id_,media_type='photo',count='100',start_from=start)
+while True:
+    print('Логин:')
+    user_login = input()
+
+    print('Пароль:')
+    user_password = input()
     try:
-        for o in range(1,len(attachments)-1):
-            links.append(attachments[str(o)]['photo']['src_big'])
-        print(attachments)
-        start = attachments['next_from']
-    except TypeError:
+        session = vk.AuthSession('5932434', user_login, user_password, scope='wall, messages')
+        vk_api = vk.API(session)
         break
-#attachments = vk_api.messages.getHistoryAttachments(peer_id=id_,media_type='photo',count=str(num % 100),start_from=start)
-#for i in range(1,len(attachments)-1):
-#    links.append(attachments[str(i)]['photo']['src_big'])
+    except vk.exceptions.VkAuthError:
+        print('Авторизация не удалась: неверный логин или пароль')
 
+while True:
+    print('Полная ссылка на беседу:')
+    link = input()
+    dialog_id = link[link.find('=')+1:]
 
-os.mkdir('Photos')
+    print('Сколько фотографий?')
+    num = int(input())
 
-os.chdir('Photos')
+    links = []
+    start = ''
+    range_ = num // 100
+    for i in range(range_):
+        attachments = vk_api.messages.getHistoryAttachments(peer_id=dialog_id,media_type='photo',count='100',start_from=start)
+        try:
+            for o in range(1,len(attachments)-1):
+                links.append(attachments[str(o)]['photo']['src_big'])
+            print(attachments)
+            start = attachments['next_from']
+        except TypeError:
+            break
 
-file_num=0
-for href in links:
-    urllib.request.urlretrieve(href, str(file_num))
-    file_num += 1
-    print ("Skachano " + str(file_num) + " failov\n")
+    file_list = os.listdir('Photos')
+    files_count = len(file_list)
+
+    try:
+        os.mkdir('Photos')
+        os.chdir('Photos')
+    except FileExistsError:
+        os.chdir('Photos')
+
+    file_num=0
+    for href in links:
+        urllib.request.urlretrieve(href, str(files_count))
+        files_count += 1
+        file_num += 1
+        print ("Скачано " + str(file_num) + " файлов\n")
+
+    print('Загрузка завершена')
+    file_num = 0
+
+    for file in file_list:
+        if file[file.rfind('.')+1:] != 'jpg':
+            os.rename(file,file+'.jpg')
+            file_num += 1
+            print("Переименовано " + str(file_num) + " файлов\n")
+
+    print("Скачать фото из других диалогов? (Да/Нет)")
+    answer = input().lower()
+    if answer == "нет":
+        break
